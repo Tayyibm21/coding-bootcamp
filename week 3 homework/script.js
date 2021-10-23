cbfd831044a635f55095a0a05a0f2e8910372ade
Tayyib Mahmood <Tayyibmahmood2001@outlook.com>
2021-10-23 14:33:30 +0100

    javascrip code

---
+++ week 3 homework/script.js
@@ -0,0 +1,59 @@
+// Assignment Code
+var generateBtn = document.querySelector('#generate');
+
+function generatePassword() {
+  // PLEASE ADD ALL YOUR CODE HERE
+}
+alert ("text");
+var text
+var intiger
+if ( text = intiger) {
+"check is within range"
+} else {
+  alert and break
+}
+
+var confirm; (lowercase)
+var confirm; (uppercase)
+var confirm; (number)
+var confirm; (special)
+
+if (if all is false) {
+  alert and break
+
+} else {
+  return criteria
+}
+
+
+
+const (uppercase) "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+const (lowercase) "abcdefghijklmnopqrstuvwxyz"
+const (number) "0123456789"
+const (special) " !"#$%&'()\*+,-./:;<=>?@[\]^\_`{|}~"
+
+function getrandomcharacter () {
+
+Array; "uppercase"
+Array; "lowercase"
+Array; "number"
+Array; "special"
+Array; "password collection"
+
+var lowercase = "password collection"
+var uppercase = "password collection"
+var number = "password collection"
+var special = "password collection"
+}
+
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector('#password');j
+
+  passwordText.value = password;
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener('click', writePassword);
